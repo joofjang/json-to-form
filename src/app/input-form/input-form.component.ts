2cdf1af0cc294a4178c7da9219e0a6014596e039
joofjang <joofjang@gmail.com>
2021-11-03 20:07:40 +0700

    Unsubscribe all rxjs subscription.

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,25 +1,29 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {FormMetaData} from "../form-meta-data.interface";
-import {FormGroupData} from "./form-group-data.interface";
+import {FormGroupData} from "../form-group-data.interface";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
   styleUrls: ['./input-form.component.css']
 })
-export class InputFormComponent implements OnChanges {
-  @Input() formTemplate: FormMetaData[] = []
+export class InputFormComponent implements OnChanges, OnDestroy {
+  @Input() formTemplate: FormMetaData[] = [];
 
   formControlGroup!: FormGroupData[];
   inputForm!: FormGroup;
+  subscriptions: Subscription[] = [];
 
   constructor() {
   }
 
   ngOnChanges(): void {
-    this.formControlGroup = []
-    this.inputForm = new FormGroup({})
+    this.unsubscribeAll();
+    this.subscriptions = [];
+    this.formControlGroup = [];
+    this.inputForm = new FormGroup({});
 
     for (const form of this.formTemplate) {
       let newControl!: FormControl
@@ -29,32 +33,43 @@ export class InputFormComponent implements OnChanges {
             this.inputForm.get(form.id)!.setValue(form.value);
             break;
           }
-          newControl = new FormControl(form.value)
-          newControl.valueChanges.subscribe(v => newControl.setValue(v, {emitEvent: false}))
-          this.inputForm.addControl(form.id, newControl)
+          newControl = new FormControl(form.value);
+          this.subscriptions.push(newControl.valueChanges.subscribe(v => newControl.setValue(v, {emitEvent: false})));
+          this.inputForm.addControl(form.id, newControl);
           this.formControlGroup.push({
             ...form,
             formControl: newControl
-          })
+          });
           break;
         case 'radio':
           if (this.inputForm.contains(form.id)) {
             this.formControlGroup.push({
               ...form,
               formControl: this.inputForm.get(form.id)! as FormControl,
-            })
+            });
             break;
           }
           newControl = new FormControl("")
-          newControl.valueChanges.subscribe(v => newControl.setValue(v, {emitEvent: false}))
-          this.inputForm.addControl(form.id, newControl)
+          this.subscriptions.push(newControl.valueChanges.subscribe(v => newControl.setValue(v, {emitEvent: false})));
+          this.inputForm.addControl(form.id, newControl);
           this.formControlGroup.push({
             ...form,
             formControl: newControl,
-          })
+          });
           break;
       }
     }
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribeAll();
+  }
+
+  private unsubscribeAll() {
+    this.subscriptions.forEach(s => {
+      if (!s.closed) {
+        s.unsubscribe();
+      }
+    });
+  }
 }
